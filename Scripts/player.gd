5a73152cd3f96f3960733d4c709d50724f0a384e
MustafaBioS <mustaafahany@gmail.com>
2025-11-28 23:27:57 +0200

    ADD - Player Functionality

---
+++ Scripts/player.gd
@@ -0,0 +1,42 @@
+extends CharacterBody3D
+
+@onready var camera = $Camera
+
+const SPEED = 10.0
+const JUMP_VELOCITY = 6.0
+var sensitivity = 0.003
+
+func player():
+	pass
+
+func _ready() -> void:
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	
+func _process(float) -> void:
+	if Input.is_action_just_pressed("esc"):
+		get_tree().quit()
+
+func _physics_process(delta: float) -> void:
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	var input_dir := Input.get_vector("a", "d", "w", "s")
+	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+
+	if direction:
+		velocity.x = direction.x * SPEED
+		velocity.z = direction.z * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.z = move_toward(velocity.z, 0, SPEED)
+
+	move_and_slide()
+
+func _unhandled_input(event):
+	if event is InputEventMouseMotion:
+		rotate_y(-event.relative.x * sensitivity)
+		camera.rotate_x(-event.relative.y * sensitivity)
+		camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-60), deg_to_rad(70))
